4517e21bb48fe351a930015b5ce39de56d318604
aarti-ar <aarti.vaja12@gmail.com>
2022-01-04 11:40:39 +0000

    added BaseTest2 file

---
+++ src/test/java/com/cslgroupservicesltd/BaseTest2.java
@@ -0,0 +1,12 @@
+package com.cslgroupservicesltd;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest2 {
+    WebDriver driver;
+
+
+    public BaseTest2(WebDriver driver){
+        this.driver=driver;
+    }
+}
